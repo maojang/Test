90fbc0806bd0ad9f6469daf2903347fdf4fe850e
Izlude <izlude@Izludes-MacBook-Air.local>
2025-04-23 10:28:10 +0700

    Server Performance Stats

--- server-stats.sh
+++ server-stats.sh
@@ -5,25 +5,25 @@ echo ""
 
 # CPU Usage
 echo "1. CPU Usage:"
-# คำสั่งแสดงการใช้ CPU
+top -l 1 -s 0 -n 5 | grep "CPU usage" | awk '{print "User: " $3 ", System: " $5 ", Idle: " $7}'
 echo ""
 
 # Memory Usage
 echo "2. Memory Usage:"
-# คำสั่งแสดงการใช้ Memory
+vm_stat | perl -ne '/page size of (\d+)/ and $size=$1; /Pages\s+([^:]+)[^\d]+(\d+)/ and printf("%-16s % 16.2f MB\n", "$1:", $2 * $size / 1048576)'
 echo ""
 
 # Disk Usage
 echo "3. Disk Usage:"
-# คำสั่งแสดงการใช้ Disk
+df -h | grep -E 'Filesystem|disk1s' | awk '{print $1 ": " $3 " used, " $4 " free"}'
 echo ""
 
-# Top 5 CPU processes
+# Top 5 Processes by CPU
 echo "4. Top 5 Processes by CPU:"
-# คำสั่งแสดงกระบวนการที่ใช้ CPU มากที่สุด
+ps -erco %cpu,comm | head -n 6
 echo ""
 
-# Top 5 Memory processes
+# Top 5 Processes by Memory
 echo "5. Top 5 Processes by Memory:"
-# คำสั่งแสดงกระบวนการที่ใช้ Memory มากที่สุด
+ps -ercmo %mem,comm | head -n 6
 echo ""
